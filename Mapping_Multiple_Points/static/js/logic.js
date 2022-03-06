6e2d29d59062fe6a40b2a79e25e950ce6f74fa84
Rus Kram <ruskram87@gmail.com>
2022-03-06 16:14:11 -0500

    mapping single points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-// Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
       40.7, -94.5
@@ -16,6 +14,12 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
